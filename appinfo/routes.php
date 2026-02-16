383b98c7c955573dd2c5c26722a6c9632d6ce29e
ernolf <raphael.gradenwitz@googlemail.com>
2026-02-16 04:59:20 +0100

    style(routes): normalize indentation
    Signed-off-by: ernolf <raphael.gradenwitz@googlemail.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,15 +2,15 @@
 
 return [
 	'routes' => [
-	['name' => 'privatePage#getByPath', 'url' => '/u/{userId}/{path}',
-		'requirements' => array('path' => '.+')],
+		['name' => 'privatePage#getByPath', 'url' => '/u/{userId}/{path}',
+			'requirements' => array('path' => '.+')],
 
-	['name' => 'pubPage#getByToken', 'url' => '/s/{token}'],
-	['name' => 'pubPage#getByTokenWithoutS', 'url' => '/{token}'],
+		['name' => 'pubPage#getByToken', 'url' => '/s/{token}'],
+		['name' => 'pubPage#getByTokenWithoutS', 'url' => '/{token}'],
 
-	['name' => 'pubPage#getByTokenAndPath', 'url' => '/s/{token}/{path}',
-		'requirements' => array('path' => '.+')],
-	['name' => 'pubPage#getByTokenAndPathWithoutS', 'url' => '/{token}/{path}',
-		'requirements' => array('path' => '.+')],
+		['name' => 'pubPage#getByTokenAndPath', 'url' => '/s/{token}/{path}',
+			'requirements' => array('path' => '.+')],
+		['name' => 'pubPage#getByTokenAndPathWithoutS', 'url' => '/{token}/{path}',
+			'requirements' => array('path' => '.+')],
 	]
 ];
